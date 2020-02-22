db2b856dd4591345805616ae521389970764c2d6
shai mahnai <shai.mahnai@gmail.com>
2020-02-22 22:44:08 +0200

    Add Date of aproval and street status to the API response

--- Server/Managers/HttpRequestsManager.cs
+++ Server/Managers/HttpRequestsManager.cs
@@ -14,22 +14,13 @@ namespace TDH.Managers
         public string Get(HttpWebRequest request)
         {
             string html = string.Empty;
-            bool success = false;
-            while (success == false)
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
             {
-
-
-
-                request.AutomaticDecompression = DecompressionMethods.GZip;
-                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-                using (Stream stream = response.GetResponseStream())
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    html = reader.ReadToEnd();
-                    success = true;
-                }
+                html = reader.ReadToEnd();
             }
-
 
             return html;
         }
